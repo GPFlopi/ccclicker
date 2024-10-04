49d83d2d6ce0b7661be2df5f14773a5f338aabe5
Bpat <40142657+GPFlopi@users.noreply.github.com>
2024-10-04 16:08:41 +0200

    Connected it to the backend server

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import CC from '/CC.svg'
+// import fetch from 'node-fetch';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+    async function Count() {
+        const response = await fetch('http://127.0.0.1:8000/counter/count/');
+        setCount(Number(await response.text()));
+    }
+    Count()
+
+    return (
     <>
       <div>
           <img src={CC} className="logo react" alt="CC"
-                onClick={() => setCount((count) => count + 1)}
+                onClick={async () => {
+                    await fetch('http://127.0.0.1:8000/counter/inc/')
+                    const response = await fetch('http://127.0.0.1:8000/counter/count/');
+                    setCount(Number(await response.text()));
+                    }
+                }
           />
       </div>
       <h1>CCCount is {count}</h1>
     </>
   )
 }
+
+
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ccclicker',
   plugins: [react()],
+  server:{
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000', // your Django server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
 })
